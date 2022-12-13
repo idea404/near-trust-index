d344f6286d44f5c84d5ec8cd4671bc345d3a77be
Dennis <10233439+idea404@users.noreply.github.com>
2022-12-13 18:59:39 +0100

    Index XCC Implementation (#6)
    * first commit
    
    * add callback logic and new data collection
    
    * index additional data container
    
    * use rubric in iodex calculation
    
    * pass array of numbers digested after rubric
    
    * update tests
    
    * update test name
    
    * temp logging
    
    * fix logic for xcc
    
    * remove unused package-lock
    
    * temp fixes to whitelist refs
    
    * remove logs from tests
    
    * add test for account listed in all contracts
    
    * refactor poe call
    
    * refactoring of callback flow
    
    * final tests and functionality

--- contract/src/index.ts
+++ contract/src/index.ts
@@ -1,49 +1,212 @@
-import { NearBindgen, near, call, view, LookupMap } from 'near-sdk-js';
-import Decimal from 'decimal.js';
+import { NearBindgen, near, call, view, LookupMap, NearPromise, UnorderedMap } from "near-sdk-js";
+import Decimal from "decimal.js";
 
 @NearBindgen({})
-class HelloNear {
-  accountIndexHistory: LookupMap<string>
-  accountIndexHistoryTimestamps: LookupMap<string>
+class NearTrustIndex {
+  accountIndexHistory: LookupMap<string>;
+  accountIndexHistoryTimestamp: LookupMap<string>;
+  accountIndexHistoryFailures: UnorderedMap<string>;
+  accountResult: UnorderedMap<bigint>;
+  whitelist: object;
 
   constructor() {
     this.accountIndexHistory = new LookupMap("aih");
-    this.accountIndexHistoryTimestamps = new LookupMap("aiht");
+    this.accountIndexHistoryTimestamp = new LookupMap("aiht");
+    this.accountIndexHistoryFailures = new UnorderedMap("aihf");
+    this.accountResult = new UnorderedMap("ar");
+    this.whitelist = near.currentAccountId() === "index.test.near" ? TEST_WHITELIST : WHITELIST;
   }
 
   @view({})
-  get_index_from_history({ account_id }: { account_id: string }): { account_id: string, index: string | null, timestamp: string | null } {
-    return { account_id: account_id, index: this.accountIndexHistory.get(account_id), timestamp: this.accountIndexHistoryTimestamps.get(account_id) };
+  get_index_from_history({ account_id }: { account_id: string }): { account_id: string; index: string | null; timestamp: string | null, errors: object[] } {
+    if (this.whitelist[account_id]) {
+      return {
+        account_id: account_id,
+        index: "1.00",
+        timestamp: near.blockTimestamp().toString(),
+        errors: []
+      };
+    }
+    return {
+      account_id: account_id,
+      index: this.accountIndexHistory.get(account_id),
+      timestamp: this.accountIndexHistoryTimestamp.get(account_id),
+      errors: getAccountErrors(this.accountIndexHistoryFailures, account_id)
+    };
   }
 
   @call({ payableFunction: true })
-  get_index({ account_id }: { account_id: string }): { account_id: string, index: string | null, timestamp: string | null } {
+  calculate_index({ account_id }: { account_id: string }): NearPromise | void {
     // TODO: require fee
-    const index = calculateIndex(account_id);
-    this.accountIndexHistory.set(account_id, index);
-    const thisTimestamp = near.blockTimestamp().toString()
-    this.accountIndexHistoryTimestamps.set(account_id, thisTimestamp);
-    return { account_id: account_id, index: index, timestamp: thisTimestamp };
+    if (this.whitelist[account_id]) {
+      return;
+    }
+    let promise = constructPromiseChainFromWhitelist(this.whitelist, account_id, BigInt("30" + "0".repeat(12)))
+    return promise.asReturn();
+  }
+
+  @call({ privateFunction: true })
+  internalCallback({ accountId }: { accountId: string }): { account_id: string; index: string | null; timestamp: string | null, errors: object[] } {
+    const callCount = near.promiseResultsCount();
+
+    let accountScores: number[] = getRawAccountScoresFromPromises(
+      accountId, this.accountIndexHistoryFailures, callCount, this.whitelist, this.accountResult
+    );
+
+    const timestamp = near.blockTimestamp().toString();
+    const accountIndex = calculateIndexFromScoresArray(accountScores);
+    near.log("accountIndex for " + accountId + ": " + accountIndex)
+    this.accountIndexHistory.set(accountId, accountIndex);
+    this.accountIndexHistoryTimestamp.set(accountId, timestamp);
+    const errors = getAccountErrors(this.accountIndexHistoryFailures, accountId);
+
+    return {
+      account_id: accountId,
+      index: accountIndex,
+      timestamp: timestamp,
+      errors: errors
+    };
   }
 }
 
-function calculateIndex(account_id: string): string {
-  let score = new Decimal(0.01);
-  if (account_id in WHITELIST) {
-    return (score.add(new Decimal(0.99))).toFixed(2);
+function getRawAccountScoresFromPromises(
+  accountId: string,
+  accountIndexHistoryFailures: UnorderedMap<string>,
+  callCount: bigint,
+  whitelist: object,
+  accountResult: UnorderedMap<bigint>
+) {
+  let accountScores: number[] = [];
+  accountIndexHistoryFailures.set(accountId, "");
+
+  for (let i = 0; i < callCount; i++) {
+    let accountName = Object.keys(whitelist)[i];
+    let accountFunctions = whitelist[accountName];
+
+    for (let j = 0; j < accountFunctions.length; j++) {
+      let functionName = accountFunctions[j];
+      let mapKey = accountId + ":" + accountName + ":" + functionName; // nested collections cumbersome: https://docs.near.org/develop/contracts/storage#map
+
+      try {
+        const promiseResult = near.promiseResult(i);
+
+        try {
+          const promiseObject = JSON.parse(promiseResult);
+          accountResult.set(mapKey, promiseObject);
+          const score = functionName == CallType.NFT_COUNT ? NFTCountRubric.getScoreFromNFTCount(promiseObject) : 0;
+          accountScores.push(score);
+          near.log("accountResult for " + mapKey + ": " + promiseObject);
+        } catch (error) {
+          const msg = "Failed saving result from successful promise for id: " + i + " with error message: " + error.message;
+          near.log(msg);
+          accountIndexHistoryFailures.set(mapKey, msg);
+        }
+
+      } catch (error) {
+        const msg = `Contract Function ${i} threw error`;
+        near.log(msg);
+        accountIndexHistoryFailures.set(mapKey, msg);
+      }
+    }
   }
-  // TODO: query whitelisted accounts for this account_id using NearPromise
-  return score.toFixed(2);
+
+  return accountScores;
 }
 
-class WhitelistTypes {
-  static readonly NFT = 'NFT';
-  static readonly DAO = 'DAO';
+function constructPromiseChainFromWhitelist(whitelist: object, accountId: string, gasAmountPerCall: bigint): NearPromise {
+  let thisContractName = Object.keys(whitelist)[0];
+  let promise = NearPromise.new(thisContractName);
+
+  for (let i = 0; i < whitelist[thisContractName].length; i++) {
+    const functionName = whitelist[thisContractName][i];
+    promise = promise.functionCall(functionName, JSON.stringify({ account_id: accountId }), BigInt(0), gasAmountPerCall);
+  }
+
+  for (let i = 1; i < Object.keys(whitelist).length; i++) {
+    thisContractName = Object.keys(whitelist)[i];
+    let newPromise = NearPromise.new(thisContractName);
+
+    for (let i = 0; i < whitelist[thisContractName].length; i++) {
+      const functionName = whitelist[thisContractName][i];
+      newPromise = newPromise.functionCall(functionName, JSON.stringify({ account_id: accountId }), BigInt(0), gasAmountPerCall);
+    }
+    promise = promise.and(newPromise);
+  }
+
+  promise = promise.then(
+    NearPromise
+      .new(near.currentAccountId())
+      .functionCall("internalCallback", JSON.stringify({ accountId: accountId }), BigInt(0), gasAmountPerCall)
+  );
+
+  return promise;
+};
+
+function calculateIndexFromScoresArray(accountScores: number[]): string {
+  let accountIndex = new Decimal(0);
+  const accountResultLength = accountScores.length;
+
+  if (accountResultLength === 0) {
+    near.log("No scores to calculate index from");
+    return "0.00";
+  }
+
+  for (let i = 0; i < accountResultLength; i++) {
+    accountIndex = accountIndex.plus(new Decimal(accountScores[i]));
+  }
+
+  accountIndex = accountIndex.dividedBy(accountResultLength);
+
+  return accountIndex.toFixed(2);
 }
 
+function getAccountErrors(accountIndexHistoryFailures: UnorderedMap<string>, accountId: string): object[] {
+  let errors: object[] = [];
+  const keysValues = accountIndexHistoryFailures.toArray().slice(1);
+  for (let i = 0; i < keysValues.length; i++) {
+    const key = keysValues[i][0];
+    if (key.includes(accountId)) {
+      const error = accountIndexHistoryFailures.get(key)
+      const errorObj = {
+        error: error,
+        contract: key.split(":")[1]
+      };
+      if (error) {
+        errors.push(errorObj);
+      }
+    }
+  }
+  return errors;
+}
+
+enum CallType {
+  NFT_COUNT = "nft_supply_for_owner",
+  NFT_ITEMS = "nft_tokens_for_owner",
+}
+
+class NFTCountRubric {
+  public static getScoreFromNFTCount(nftCount: number): number {
+    if (nftCount === 0) {
+      return 0;
+    }
+    if (nftCount >= 1) {
+      return 1;
+    }
+    return 0;
+  }
+}
+
+// TODO: implement xcc logic for NFT_ITEMS
 const WHITELIST = {
-  "asac.near": WhitelistTypes.NFT,
-  "nearnautnft.near": WhitelistTypes.NFT,
-  "secretskelliessociety.near": WhitelistTypes.NFT,
-  "kycdao.near": WhitelistTypes.DAO,
-}
+  "asac.near": [CallType.NFT_COUNT],
+  "nearnautnft.near": [CallType.NFT_COUNT],
+  "secretskelliessociety.near": [CallType.NFT_COUNT],
+  "kycdao.near": [],
+};
+
+const TEST_WHITELIST = {
+  "asac.test.near": [CallType.NFT_COUNT],
+  "nearnautnft.test.near": [CallType.NFT_COUNT],
+  "secretskelliessociety.test.near": [CallType.NFT_COUNT],
+  "kycdao.test.near": [],
+};
