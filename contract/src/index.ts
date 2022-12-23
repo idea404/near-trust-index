adb558ae63989888048c2fc2d84075e46c4ef737
Dennis <10233439+idea404@users.noreply.github.com>
2022-12-23 14:40:57 +0100

    Develop (#11)
    * Adding linting rules
    
    * Adding prettier
    
    * Adding tsconfig file
    
    * Adding linting & prettier rules
    
    * Adding formatting and linting actions
    
    * Formatting and linting
    
    * Develop master (#10)
    
    * Index XCC Implementation (#6)
    
    * first commit
    
    * add callback logic and new data collection
    
    * index additional data container
    
    * use rubric in iodex calculation
    
    * pass array of numbers digested after rubric
    
    * update tests
    
    * update test name
    
    * temp logging
    
    * fix logic for xcc
    
    * remove unused package-lock
    
    * temp fixes to whitelist refs
    
    * remove logs from tests
    
    * add test for account listed in all contracts
    
    * refactor poe call
    
    * refactoring of callback flow
    
    * final tests and functionality
    
    * update package lock json
    
    * unlint repo
    
    * update package.json
    
    * update package.json
    
    * run format
    
    * remove unused plock json file
    
    * config CI runs
    
    * remove duplicate code line
    
    Co-authored-by: Sandoche <sandoche@protonmail.com>

--- contract/src/index.ts
+++ contract/src/index.ts
@@ -1,37 +1,42 @@
-import { NearBindgen, near, call, view, LookupMap, NearPromise, UnorderedMap } from "near-sdk-js";
-import Decimal from "decimal.js";
+import Decimal from 'decimal.js';
+import { NearBindgen, near, call, view, LookupMap, NearPromise, UnorderedMap } from 'near-sdk-js';
 
 @NearBindgen({})
 class NearTrustIndex {
   accountIndexHistory: LookupMap<string>;
+
   accountIndexHistoryTimestamp: LookupMap<string>;
+
   accountIndexHistoryFailures: UnorderedMap<string>;
+
   accountResult: UnorderedMap<bigint>;
+
   whitelist: object;
 
   constructor() {
-    this.accountIndexHistory = new LookupMap("aih");
-    this.accountIndexHistoryTimestamp = new LookupMap("aiht");
-    this.accountIndexHistoryFailures = new UnorderedMap("aihf");
-    this.accountResult = new UnorderedMap("ar");
-    this.whitelist = near.currentAccountId() === "index.test.near" ? TEST_WHITELIST : WHITELIST;
+    this.accountIndexHistory = new LookupMap('aih');
+    this.accountIndexHistoryTimestamp = new LookupMap('aiht');
+    this.accountIndexHistoryFailures = new UnorderedMap('aihf');
+    this.accountResult = new UnorderedMap('ar');
+    this.whitelist = near.currentAccountId() === 'index.test.near' ? TEST_WHITELIST : WHITELIST;
   }
 
   @view({})
-  get_index_from_history({ account_id }: { account_id: string }): { account_id: string; index: string | null; timestamp: string | null, errors: object[] } {
+  get_index_from_history({ account_id }: { account_id: string }): { account_id: string; errors: object[]; index: string | null; timestamp: string | null } {
     if (this.whitelist[account_id]) {
       return {
-        account_id: account_id,
-        index: "1.00",
+        account_id,
+        errors: [],
+        index: '1.00',
         timestamp: near.blockTimestamp().toString(),
-        errors: []
       };
     }
+
     return {
-      account_id: account_id,
+      account_id,
+      errors: getAccountErrors(this.accountIndexHistoryFailures, account_id),
       index: this.accountIndexHistory.get(account_id),
       timestamp: this.accountIndexHistoryTimestamp.get(account_id),
-      errors: getAccountErrors(this.accountIndexHistoryFailures, account_id)
     };
   }
 
@@ -41,30 +46,29 @@ class NearTrustIndex {
     if (this.whitelist[account_id]) {
       return;
     }
-    let promise = constructPromiseChainFromWhitelist(this.whitelist, account_id, BigInt("30" + "0".repeat(12)))
+
+    const promise = constructPromiseChainFromWhitelist(this.whitelist, account_id, BigInt('30' + '0'.repeat(12)));
     return promise.asReturn();
   }
 
   @call({ privateFunction: true })
-  internalCallback({ accountId }: { accountId: string }): { account_id: string; index: string | null; timestamp: string | null, errors: object[] } {
+  internalCallback({ accountId }: { accountId: string }): { account_id: string; errors: object[]; index: string | null; timestamp: string | null } {
     const callCount = near.promiseResultsCount();
 
-    let accountScores: number[] = getRawAccountScoresFromPromises(
-      accountId, this.accountIndexHistoryFailures, callCount, this.whitelist, this.accountResult
-    );
+    const accountScores: number[] = getRawAccountScoresFromPromises(accountId, this.accountIndexHistoryFailures, callCount, this.whitelist, this.accountResult);
 
     const timestamp = near.blockTimestamp().toString();
     const accountIndex = calculateIndexFromScoresArray(accountScores);
-    near.log("accountIndex for " + accountId + ": " + accountIndex)
+    near.log('accountIndex for ' + accountId + ': ' + accountIndex);
     this.accountIndexHistory.set(accountId, accountIndex);
     this.accountIndexHistoryTimestamp.set(accountId, timestamp);
     const errors = getAccountErrors(this.accountIndexHistoryFailures, accountId);
 
     return {
       account_id: accountId,
+      errors,
       index: accountIndex,
-      timestamp: timestamp,
-      errors: errors
+      timestamp,
     };
   }
 }
@@ -74,38 +78,36 @@ function getRawAccountScoresFromPromises(
   accountIndexHistoryFailures: UnorderedMap<string>,
   callCount: bigint,
   whitelist: object,
-  accountResult: UnorderedMap<bigint>
+  accountResult: UnorderedMap<bigint>,
 ) {
-  let accountScores: number[] = [];
-  accountIndexHistoryFailures.set(accountId, "");
+  const accountScores: number[] = [];
+  accountIndexHistoryFailures.set(accountId, '');
 
-  for (let i = 0; i < callCount; i++) {
-    let accountName = Object.keys(whitelist)[i];
-    let accountFunctions = whitelist[accountName];
+  for (let index = 0; index < callCount; index++) {
+    const accountName = Object.keys(whitelist)[index];
+    const accountFunctions = whitelist[accountName];
 
-    for (let j = 0; j < accountFunctions.length; j++) {
-      let functionName = accountFunctions[j];
-      let mapKey = accountId + ":" + accountName + ":" + functionName; // nested collections cumbersome: https://docs.near.org/develop/contracts/storage#map
+    for (const functionName of accountFunctions) {
+      const mapKey = accountId + ':' + accountName + ':' + functionName; // nested collections cumbersome: https://docs.near.org/develop/contracts/storage#map
 
       try {
-        const promiseResult = near.promiseResult(i);
+        const promiseResult = near.promiseResult(index);
 
         try {
           const promiseObject = JSON.parse(promiseResult);
           accountResult.set(mapKey, promiseObject);
           const score = functionName == CallType.NFT_COUNT ? NFTCountRubric.getScoreFromNFTCount(promiseObject) : 0;
           accountScores.push(score);
-          near.log("accountResult for " + mapKey + ": " + promiseObject);
+          near.log('accountResult for ' + mapKey + ': ' + promiseObject);
         } catch (error) {
-          const msg = "Failed saving result from successful promise for id: " + i + " with error message: " + error.message;
-          near.log(msg);
-          accountIndexHistoryFailures.set(mapKey, msg);
+          const message = 'Failed saving result from successful promise for id: ' + index + ' with error message: ' + error.message;
+          near.log(message);
+          accountIndexHistoryFailures.set(mapKey, message);
         }
-
-      } catch (error) {
-        const msg = `Contract Function ${i} threw error`;
-        near.log(msg);
-        accountIndexHistoryFailures.set(mapKey, msg);
+      } catch {
+        const message = `Contract Function ${index} threw error`;
+        near.log(message);
+        accountIndexHistoryFailures.set(mapKey, message);
       }
     }
   }
@@ -117,42 +119,39 @@ function constructPromiseChainFromWhitelist(whitelist: object, accountId: string
   let thisContractName = Object.keys(whitelist)[0];
   let promise = NearPromise.new(thisContractName);
 
-  for (let i = 0; i < whitelist[thisContractName].length; i++) {
-    const functionName = whitelist[thisContractName][i];
+  for (let index = 0; index < whitelist[thisContractName].length; index++) {
+    const functionName = whitelist[thisContractName][index];
     promise = promise.functionCall(functionName, JSON.stringify({ account_id: accountId }), BigInt(0), gasAmountPerCall);
   }
 
-  for (let i = 1; i < Object.keys(whitelist).length; i++) {
-    thisContractName = Object.keys(whitelist)[i];
+  for (let index = 1; index < Object.keys(whitelist).length; index++) {
+    thisContractName = Object.keys(whitelist)[index];
     let newPromise = NearPromise.new(thisContractName);
 
-    for (let i = 0; i < whitelist[thisContractName].length; i++) {
-      const functionName = whitelist[thisContractName][i];
+    for (let index = 0; index < whitelist[thisContractName].length; index++) {
+      const functionName = whitelist[thisContractName][index];
       newPromise = newPromise.functionCall(functionName, JSON.stringify({ account_id: accountId }), BigInt(0), gasAmountPerCall);
     }
+
     promise = promise.and(newPromise);
   }
 
-  promise = promise.then(
-    NearPromise
-      .new(near.currentAccountId())
-      .functionCall("internalCallback", JSON.stringify({ accountId: accountId }), BigInt(0), gasAmountPerCall)
-  );
+  promise = promise.then(NearPromise.new(near.currentAccountId()).functionCall('internalCallback', JSON.stringify({ accountId }), BigInt(0), gasAmountPerCall));
 
   return promise;
-};
+}
 
 function calculateIndexFromScoresArray(accountScores: number[]): string {
   let accountIndex = new Decimal(0);
   const accountResultLength = accountScores.length;
 
   if (accountResultLength === 0) {
-    near.log("No scores to calculate index from");
-    return "0.00";
+    near.log('No scores to calculate index from');
+    return '0.00';
   }
 
-  for (let i = 0; i < accountResultLength; i++) {
-    accountIndex = accountIndex.plus(new Decimal(accountScores[i]));
+  for (let index = 0; index < accountResultLength; index++) {
+    accountIndex = accountIndex.plus(new Decimal(accountScores[index]));
   }
 
   accountIndex = accountIndex.dividedBy(accountResultLength);
@@ -161,27 +160,28 @@ function calculateIndexFromScoresArray(accountScores: number[]): string {
 }
 
 function getAccountErrors(accountIndexHistoryFailures: UnorderedMap<string>, accountId: string): object[] {
-  let errors: object[] = [];
+  const errors: object[] = [];
   const keysValues = accountIndexHistoryFailures.toArray().slice(1);
-  for (let i = 0; i < keysValues.length; i++) {
-    const key = keysValues[i][0];
+  for (const keysValue of keysValues) {
+    const key = keysValue[0];
     if (key.includes(accountId)) {
-      const error = accountIndexHistoryFailures.get(key)
-      const errorObj = {
-        error: error,
-        contract: key.split(":")[1]
+      const error = accountIndexHistoryFailures.get(key);
+      const errorObject = {
+        contract: key.split(':')[1],
+        error,
       };
       if (error) {
-        errors.push(errorObj);
+        errors.push(errorObject);
       }
     }
   }
+
   return errors;
 }
 
 enum CallType {
-  NFT_COUNT = "nft_supply_for_owner",
-  NFT_ITEMS = "nft_tokens_for_owner",
+  NFT_COUNT = 'nft_supply_for_owner',
+  NFT_ITEMS = 'nft_tokens_for_owner',
 }
 
 class NFTCountRubric {
@@ -189,24 +189,26 @@ class NFTCountRubric {
     if (nftCount === 0) {
       return 0;
     }
+
     if (nftCount >= 1) {
       return 1;
     }
+
     return 0;
   }
 }
 
 // TODO: implement xcc logic for NFT_ITEMS
 const WHITELIST = {
-  "asac.near": [CallType.NFT_COUNT],
-  "nearnautnft.near": [CallType.NFT_COUNT],
-  "secretskelliessociety.near": [CallType.NFT_COUNT],
-  "kycdao.near": [],
+  'asac.near': [CallType.NFT_COUNT],
+  'kycdao.near': [],
+  'nearnautnft.near': [CallType.NFT_COUNT],
+  'secretskelliessociety.near': [CallType.NFT_COUNT],
 };
 
 const TEST_WHITELIST = {
-  "asac.test.near": [CallType.NFT_COUNT],
-  "nearnautnft.test.near": [CallType.NFT_COUNT],
-  "secretskelliessociety.test.near": [CallType.NFT_COUNT],
-  "kycdao.test.near": [],
+  'asac.test.near': [CallType.NFT_COUNT],
+  'kycdao.test.near': [],
+  'nearnautnft.test.near': [CallType.NFT_COUNT],
+  'secretskelliessociety.test.near': [CallType.NFT_COUNT],
 };
--- contract/test/main.ava.js
+++ contract/test/main.ava.js
@@ -1,93 +1,93 @@
-import { Worker, NEAR } from "near-workspaces";
-import test from "ava";
+import test from 'ava';
+import { Worker, NEAR } from 'near-workspaces';
 
 test.beforeEach(async (t) => {
   const worker = await Worker.init();
   const root = worker.rootAccount;
 
-  const indexContract = await root.createSubAccount("index");
-  await indexContract.deploy("./build/index.wasm");
+  const indexContract = await root.createSubAccount('index');
+  await indexContract.deploy('./build/index.wasm');
   // TODO: import whitelist
-  const asac = await root.createSubAccount("asac");
-  const nearnaut = await root.createSubAccount("nearnautnft");
-  const secret = await root.createSubAccount("secretskelliessociety");
-  const kyc = await root.createSubAccount("kyc");
-  await asac.deploy("./build/test.wasm");
-  await nearnaut.deploy("./build/test.wasm");
-  await secret.deploy("./build/test.wasm");
-  await kyc.deploy("./build/test.wasm");
+  const asac = await root.createSubAccount('asac');
+  const nearnaut = await root.createSubAccount('nearnautnft');
+  const secret = await root.createSubAccount('secretskelliessociety');
+  const kyc = await root.createSubAccount('kyc');
+  await asac.deploy('./build/test.wasm');
+  await nearnaut.deploy('./build/test.wasm');
+  await secret.deploy('./build/test.wasm');
+  await kyc.deploy('./build/test.wasm');
 
-  const alice = await root.createSubAccount("alice", {
-    initialBalance: NEAR.parse("100 N").toJSON(),
+  const alice = await root.createSubAccount('alice', {
+    initialBalance: NEAR.parse('100 N').toJSON(),
   });
 
-  const owner = await root.createSubAccount("owner", {
-    initialBalance: NEAR.parse("100 N").toJSON(),
+  const owner = await root.createSubAccount('owner', {
+    initialBalance: NEAR.parse('100 N').toJSON(),
   });
-  const random = await root.createSubAccount("random", {
-    initialBalance: NEAR.parse("100 N").toJSON(),
+  const random = await root.createSubAccount('random', {
+    initialBalance: NEAR.parse('100 N').toJSON(),
   });
-  const errors = await root.createSubAccount("errors", {
-    initialBalance: NEAR.parse("100 N").toJSON(),
+  const errors = await root.createSubAccount('errors', {
+    initialBalance: NEAR.parse('100 N').toJSON(),
   });
 
   t.context.worker = worker;
-  t.context.accounts = { root, alice, owner, random, errors, indexContract };
+  t.context.accounts = { alice, errors, indexContract, owner, random, root };
 });
 
 test.afterEach.always(async (t) => {
   await t.context.worker.tearDown().catch((error) => {
-    console.log("Failed to tear down the worker:", error);
+    console.log('Failed to tear down the worker:', error);
   });
 });
 
-test("should return null if account not in whitelist and not in history", async (t) => {
+test('should return null if account not in whitelist and not in history', async (t) => {
   const { alice, indexContract } = t.context.accounts;
-  const result = await alice.call(indexContract, "get_index_from_history", { account_id: alice.accountId });
+  const result = await alice.call(indexContract, 'get_index_from_history', { account_id: alice.accountId });
   t.is(result.index, null);
   t.is(result.account_id, alice.accountId);
 });
 
-test("should return 1.00 if account in whitelist", async (t) => {
+test('should return 1.00 if account in whitelist', async (t) => {
   // TODO: import testnet whitelist
-  const wlAccount = "asac.test.near";
+  const wlAccount = 'asac.test.near';
   const { alice, indexContract } = t.context.accounts;
-  const result = await alice.call(indexContract, "get_index_from_history", { account_id: wlAccount });
-  t.is(result.index, "1.00");
+  const result = await alice.call(indexContract, 'get_index_from_history', { account_id: wlAccount });
+  t.is(result.index, '1.00');
   t.is(result.account_id, wlAccount);
 });
 
-test("should return a value between 0.00 and 1.00 for random account", async (t) => {
+test('should return a value between 0.00 and 1.00 for random account', async (t) => {
   const { random, indexContract } = t.context.accounts;
-  await random.call(indexContract, "calculate_index", { account_id: random.accountId }, { gas: "300" + "0".repeat(12), attachedDeposit: "1" });
-  const result = await random.call(indexContract, "get_index_from_history", { account_id: random.accountId });
-  const index = parseFloat(result.index);
+  await random.call(indexContract, 'calculate_index', { account_id: random.accountId }, { attachedDeposit: '1', gas: '300' + '0'.repeat(12) });
+  const result = await random.call(indexContract, 'get_index_from_history', { account_id: random.accountId });
+  const index = Number.parseFloat(result.index);
   t.true(index >= 0);
   t.true(index <= 1);
   t.is(result.account_id, random.accountId);
 });
 
-test("should return a list of objects in the errors key of the result", async (t) => {
+test('should return a list of objects in the errors key of the result', async (t) => {
   const { errors, indexContract } = t.context.accounts;
-  await errors.call(indexContract, "calculate_index", { account_id: errors.accountId }, { gas: "300" + "0".repeat(12), attachedDeposit: "1" });
-  const result = await errors.call(indexContract, "get_index_from_history", { account_id: errors.accountId });
+  await errors.call(indexContract, 'calculate_index', { account_id: errors.accountId }, { attachedDeposit: '1', gas: '300' + '0'.repeat(12) });
+  const result = await errors.call(indexContract, 'get_index_from_history', { account_id: errors.accountId });
   t.true(Array.isArray(result.errors));
   t.true(result.errors.length > 0);
   t.is(result.account_id, errors.accountId);
 });
 
-test("should return 0.00 if account with no transactions is not in whitelist and is in history", async (t) => {
+test('should return 0.00 if account with no transactions is not in whitelist and is in history', async (t) => {
   const { alice, indexContract } = t.context.accounts;
-  await alice.call(indexContract, "calculate_index", { account_id: alice.accountId }, { gas: "300" + "0".repeat(12), attachedDeposit: "1" });
-  const result = await alice.call(indexContract, "get_index_from_history", { account_id: alice.accountId });
-  t.is(result.index, "0.00");
+  await alice.call(indexContract, 'calculate_index', { account_id: alice.accountId }, { attachedDeposit: '1', gas: '300' + '0'.repeat(12) });
+  const result = await alice.call(indexContract, 'get_index_from_history', { account_id: alice.accountId });
+  t.is(result.index, '0.00');
   t.is(result.account_id, alice.accountId);
 });
 
-test("should return 1.00 for account with at least 1 NFT for all whitelisted accounts with functions", async (t) => {
+test('should return 1.00 for account with at least 1 NFT for all whitelisted accounts with functions', async (t) => {
   const { owner, indexContract } = t.context.accounts;
-  await owner.call(indexContract, "calculate_index", { account_id: owner.accountId }, { gas: "300" + "0".repeat(12), attachedDeposit: "1" });
-  const result = await owner.call(indexContract, "get_index_from_history", { account_id: owner.accountId });
-  t.is(result.index, "1.00");
+  await owner.call(indexContract, 'calculate_index', { account_id: owner.accountId }, { attachedDeposit: '1', gas: '300' + '0'.repeat(12) });
+  const result = await owner.call(indexContract, 'get_index_from_history', { account_id: owner.accountId });
+  t.is(result.index, '1.00');
   t.is(result.account_id, owner.accountId);
 });
